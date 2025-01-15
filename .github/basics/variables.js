d1c07e900d59ea8e868263277c41336e641e2709
Nikhil9025 <nikhil.tisangikar@gmail.com>
2025-01-15 12:31:53 +0000

    container commit

---
+++ .github/basics/variables.js
@@ -0,0 +1,22 @@
+
+const accountId = 902526; 
+let accountMail = " Nikhil@gmail.com";
+var accountPassword = "90259026";
+accountcity = "Pune";
+
+
+
+accountMail = "json@gamil.com";
+accountPassword = "216655";
+accountcity = "Mumbai";
+
+let accountState;
+
+console.table([accountId, accountMail, accountPassword, accountcity , accountState]);
+
+/*
+
+prefer not to use var 
+beacuse of issu in block scope and funcyional scope
+
+*/
