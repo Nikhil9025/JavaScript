3dcf7d789036056762ce6b33dc560bfb2716511e
Nikhil9025 <nikhil.tisangikar@gmail.com>
2025-01-15 14:05:49 +0000

    container commit

---
+++ .github/basics/DataType_1.js
@@ -0,0 +1,47 @@
+
+
+"use strict"; // treat all js code as newer   js / version .
+
+// alert("Welcome to the NT world");
+
+/*
+
+     the output will be alert notification in the starting of a websites.
+     but here compiler shows error beacause we type this code in node.js not on browser .
+     when we will type this code in console of a browser then there it will shows the output .
+
+     we can run alert and able to get output like on web but ther is a another way which we will 
+     see later .
+
+     always follow readability should be high. 
+
+*/
+
+    console.log("Human");
+
+    let name = "Nikhil"
+    let age = 24
+    let isLoggedIn = true
+
+    
+
+    let value = 25
+  
+
+    // number  =>  2 to  power of 53 
+    // for large numbers use  bigint
+    // string   => ""  for strings 
+    // boolean  =>  true/false
+    // null => standalone value
+    // undefined => 
+    // symbol => unique
+
+    //object  
+
+    console.log(typeof "Nikhil");
+    console.log(typeof age);
+
+    console.log(typeof null); // output is object . 
+    console.log(typeof undefined);  // output is undefined .
+
+    console.log(typeof null); // output is object . 
